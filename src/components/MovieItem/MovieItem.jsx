e1870ab348a5688dad8d8846d9e5aed34b3498e7
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-07 18:51:13 -0500

    add MovieItem component

---
+++ src/components/MovieItem/MovieItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function MovieItem(movie) {
+
+    return (
+        <>
+            <div key={movie.id} >
+                <h3>{movie.title}</h3>
+                <img src={movie.poster} alt={movie.title} />
+            </div>
+        </>
+    )
+}
+
+
+
+export default MovieItem;
