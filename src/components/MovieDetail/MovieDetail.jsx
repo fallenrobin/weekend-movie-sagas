985cf0c3b99c924816a9964a57403d6fc00ec117
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-09 20:14:08 -0500

    change button styling MovieDetail

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -65,7 +65,7 @@ function MovieDetail() {
                                 })}
 
                             </div>
-                            <Button style={{width:'15%', height:'10%', fontWeight: 'bold'}} variant="contained" onClick={() => { history.push('/') }}>Return to list</Button>
+                            <Button style={{width:'15%'}} variant="outlined" color="primary" onClick={() => { history.push('/') }}>Return to list</Button>
                         </CardContent>
                     </Card>
                 </Grid>
